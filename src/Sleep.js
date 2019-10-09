dc48ce3f50c705afcb8e61169c45c7289d0a5e03
Colin Turing <colin.koga@gmail.com>
2019-10-09 17:24:45 -0600

    Add methods one for grouping user data and one to return average hours slept

--- src/Sleep.js
+++ src/Sleep.js
@@ -3,9 +3,21 @@ class Sleep {
     this.sleepData = sleepData;
   }
 
+  getUserData(id) {
+    return this.sleepData.filter(data => data.userID === id);
+  };
+
+  getAverageHoursSlept(id) {
+    let userSleepData = this.getUserData(id);
+    
+    return (userSleepData.reduce((acc, day) => {
+      acc += day.hoursSlept;
+      return acc
+    }, 0) / userSleepData.length).toFixed(1)
+  }
 
 };
 
-if (typeof module !== 'undefined') {
+if (typeof module !== "undefined") {
   module.exports = Sleep;
-}
+};
--- test/sleep-test.js
+++ test/sleep-test.js
@@ -18,4 +18,24 @@ describe('Sleep', () => {
   it('should be an instanceof Sleep', () => {
     expect(sleep).to.be.an.instanceof(Sleep);
   });
+
+  it('should be able to group user information', () => {
+    expect(sleep.getUserData(1)).to.eql([
+      { userID: 1, date: '2019/06/15', hoursSlept: 6.1, sleepQuality: 2.2 },
+      { userID: 1, date: '2019/06/16', hoursSlept: 4.1, sleepQuality: 3.8 },
+      { userID: 1, date: '2019/06/17', hoursSlept: 8, sleepQuality: 2.6 },
+      { userID: 1, date: '2019/06/18', hoursSlept: 10.4, sleepQuality: 3.1},
+      { userID: 1, date: '2019/06/19', hoursSlept: 10.7, sleepQuality: 1.2},
+      { userID: 1, date: '2019/06/20', hoursSlept: 9.3, sleepQuality: 1.2 },
+      { userID: 1, date: '2019/06/21', hoursSlept: 7.8, sleepQuality: 4.2 },
+      { userID: 1, date: '2019/06/22', hoursSlept: 7, sleepQuality: 3 },
+      { userID: 1, date: '2019/06/23', hoursSlept: 7.8, sleepQuality: 1.5 },
+      { userID: 1, date: '2019/06/24', hoursSlept: 8, sleepQuality: 1.3 },
+      { userID: 1, date: '2019/06/25', hoursSlept: 5.1, sleepQuality: 3.7 }
+    ])
+  })
+
+  it('should be able to get average sleep hours per day for a user', () => {
+    expect(sleep.getAverageHoursSlept(1)).to.equal('7.7')
+  });
 });
