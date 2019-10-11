b71fa4a68fa42f16351a1d1fa1106dd91611a970
Colin Turing <colin.koga@gmail.com>
2019-10-10 18:15:44 -0600

    Add method and test to get userthat had sleep qual higher than 3 for a week

--- src/Sleep-repository.js
+++ src/Sleep-repository.js
@@ -10,7 +10,27 @@ class SleepRepo {
     }, 0) / this.data.length).toFixed(1);
   };
 
-  
+  getGreatSleepers(date) {
+    let day = this.data.findIndex(day => day.date === date);
+
+    let week = this.data.slice(day - 40, day + 30);
+
+    let usersSleepQual = week.reduce((acc, curr) => {
+      if (!acc[curr.userID]) {
+        acc[curr.userID] = [curr.sleepQuality]
+      } else {
+        acc[curr.userID].push(curr.sleepQuality)
+      }
+      return acc;
+    }, {})
+    return usersSleepQual
+    // Object.values(usersSleepQual).forEach(user => {
+    //   if (user.forEach(num => num > 3)) {
+
+    //   }
+    // })
+  }
+
 };
 
 if (typeof module !== 'undefined') {
