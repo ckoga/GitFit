3e3054208d30a009981de959aa1282eec61e7270
Colin Turing <colin.koga@gmail.com>
2019-10-14 17:27:27 -0600

    Add method and test for calculating average stairs climbed for a given date

--- src/Activity-repository.js
+++ src/Activity-repository.js
@@ -3,14 +3,17 @@ class ActivityRepo {
     this.data = activityData;
   }
 
+  getDay(date) {
+   return this.data.filter(day => day.date === date);
+  }
 
   getAvgStairsClimbed(date) {
-  let day = this.data.filter(day => day.date === date);
+  let day = this.getDay(date);
 
   return (day.reduce((acc, user) => acc += user.flightsOfStairs , 0) / day.length).toFixed(1);
   };
 
-  
+  getAvgStepsTaken(date) {}
 }
 
 if (typeof module !== 'undefined') {
--- test/activity-repository-test.js
+++ test/activity-repository-test.js
@@ -20,6 +20,21 @@ describe('Activity-repo', () => {
     expect(activityRepo).to.be.an.instanceof(ActivityRepo);
   });
 
+  it('should be able to get all data for a certain date', () => {
+    expect(activityRepo.getDay('2019/06/20')).to.eql([
+      { userID: 1, date: '2019/06/20', numSteps: 14478, minutesActive: 140, flightsOfStairs: 12 },
+      { userID: 2, date: '2019/06/20', numSteps: 8153, minutesActive: 74, flightsOfStairs: 10 },
+      { userID: 3, date: '2019/06/20', numSteps: 5369, minutesActive: 129, flightsOfStairs: 46 },
+      { userID: 4, date: '2019/06/20', numSteps: 3314, minutesActive: 240, flightsOfStairs: 6 },
+      { userID: 5, date: '2019/06/20', numSteps: 4760, minutesActive: 219, flightsOfStairs: 46 },
+      { userID: 6, date: '2019/06/20', numSteps: 13316, minutesActive: 167, flightsOfStairs: 24 },
+      { userID: 7, date: '2019/06/20', numSteps: 13795, minutesActive: 286, flightsOfStairs: 31 },
+      { userID: 8, date: '2019/06/20', numSteps: 5680, minutesActive: 137, flightsOfStairs: 2 },
+      { userID: 9, date: '2019/06/20', numSteps: 14764, minutesActive: 125, flightsOfStairs: 30 },
+      { userID: 10, date: '2019/06/20', numSteps: 3487, minutesActive: 289, flightsOfStairs: 48 }
+    ]);
+  });
+
   it('should be able to calculate the average number of stairs climbed for a specific date', () => {
     expect(activityRepo.getAvgStairsClimbed('2019/06/20')).to.equal('25.5');
   });
